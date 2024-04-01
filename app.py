d50c4e3e7e639e6875ca508aebffd20212595417
Rahul Jadhav <rahuljadhav0417@gmial.com>
2024-04-01 12:31:06 +0530

    change

--- app.py
+++ app.py
@@ -25,10 +25,10 @@ def generate_qr():
         logo_path = request.files['logo']
         logo_img = Image.open(logo_path)
         max_logo_size = min(img.width, img.height) // 2
-        resize_factor = 0.2
+        resize_factor = 0.4
         new_size = (int(logo_img.width * resize_factor), int(logo_img.height * resize_factor))
         logo_img = logo_img.resize(new_size)
-        shift_factor = 5.7
+        shift_factor = 5.1
         paste_position = ((img.width - logo_img.width) // 2 + int(img.width * shift_factor),
                           (img.height - logo_img.height) // 2)
         img.paste(logo_img, paste_position)
